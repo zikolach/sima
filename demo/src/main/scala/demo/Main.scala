c0fde1565312e9194846aa1acdacae70ff154a7d
Nikolay Kushin <nikolay.kushin@prewave.ai>
2024-03-07 10:17:36 +0100

    Add form to check resize

--- demo/src/main/scala/demo/Main.scala
+++ demo/src/main/scala/demo/Main.scala
@@ -9,6 +9,7 @@ import org.apache.pekko.http.scaladsl.model._
 import org.apache.pekko.http.scaladsl.server.Directives._
 import org.apache.pekko.stream.Materializer
 import sima.SimaClient
+import sima.api.Parameters
 
 import scala.concurrent.Future
 import scala.io.StdIn
@@ -47,6 +48,33 @@ object Main {
                   implicit val config = sima.ClientConfig()
                   SimaClient.client[IO].use(_.info(bytes)).unsafeToFuture().map { info =>
                     HttpEntity(ContentTypes.`application/json`, info.asJson.noSpaces)
+                  }
+                }
+              }
+            }
+        }
+      }
+    } ~ path("resize") {
+      get {
+        complete {
+          import cats.effect.unsafe.implicits.global
+          implicit val config = sima.ClientConfig()
+          SimaClient.client[IO].use(_.health).unsafeToFuture().map { b =>
+            HttpEntity(b.toString)
+          }
+        }
+      } ~ post {
+        fileUpload("file") {
+          case (metadata, byteSource) =>
+            extractRequestContext { ctx =>
+              implicit val mat: Materializer = ctx.materializer
+              val fileBytes: Future[Array[Byte]] = byteSource.map(_.iterator.toArray).runFold(Array.empty[Byte])((acc, next) => acc.appendedAll(next))
+              onSuccess(fileBytes) { bytes =>
+                complete {
+                  import cats.effect.unsafe.implicits.global
+                  implicit val config = sima.ClientConfig()
+                  SimaClient.client[IO].use(_.resize(bytes)(Parameters(`type` = Some("png"), width = Some(200)))).unsafeToFuture().map { resized =>
+                    HttpEntity(ContentType(MediaTypes.`image/png`), resized)
                   }
                 }
               }
--- demo/src/main/scala/demo/templates/MainPage.scala
+++ demo/src/main/scala/demo/templates/MainPage.scala
@@ -13,13 +13,21 @@ object MainPage {
           tag("title")("Sample form"),
         ),
         body(
+          h1("Demo"),
           form(
             method := "POST",
             action := "/info",
             enctype := "multipart/form-data",
             input(`type` := "file", name := "file"),
-            input(`type` := "submit", value := "Upload")
-          )
+            input(`type` := "submit", value := "Info")
+          ),
+          form(
+            method := "POST",
+            action := "/resize",
+            enctype := "multipart/form-data",
+            input(`type` := "file", name := "file"),
+            input(`type` := "submit", value := "Convert and resize")
+          ),
 //          script(`type` := "text/javascript", src := "")
         )
       ).toString
